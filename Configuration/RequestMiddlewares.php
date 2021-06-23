7576ff540501337d3bffc363c78c14518e11059a
SvenJuergens <SvenJuergens@users.noreply.github.com>
2021-06-23 09:52:13 +0200

    [TASK] compatibility with 10.4

--- Configuration/RequestMiddlewares.php
+++ Configuration/RequestMiddlewares.php
@@ -9,12 +9,7 @@ return [
             'target' => \SvenJuergens\Miniredirect\Http\Middleware\MiniRedirect::class,
             'before' => [
                 'typo3/cms-frontend/page-resolver',
-            ],
-            'after' => [
-                'typo3/cms-frontend/tsfe',
-                'typo3/cms-frontend/authentication',
-                'typo3/cms-frontend/static-route-resolver',
-            ],
+            ]
         ],
     ],
 ];
